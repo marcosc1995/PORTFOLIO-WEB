142ef13b9e539b5e8fd5915e58f414cc107ab525
Marcos <marcos_c95@outlook.com>
2022-04-20 02:54:25 -0300

    update

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ window.addEventListener('scroll', function () {
         content.classList.remove('active')
     }
 })
+
+let test = 'fifa'
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com',
+		'X-RapidAPI-Key': '6150e4d77bmsh04abafa6114ef03p1ce7e0jsncc8a1386169e'
+	}
+};
+
+fetch('https://rawg-video-games-database.p.rapidapi.com/games?key=f62da0af1d41486894d3adad81cbd732&platforms=15&page_size=25', options)
+	.then(response => response.json())
+	.then(response => test = response)
+	.catch(err => console.error(err));
